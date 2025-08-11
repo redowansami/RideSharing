febc0bdbdbbeaf20f116d67f765efa94bf813bb1
Shaikh1828 <126088347+Shaikh1828@users.noreply.github.com>
2025-08-11 22:46:29 +0600

    test removed

--- backend-test/src/main/java/com/lifeblood/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.lifeblood.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class ApplicationConfig extends Application {
-    // No implementation needed; serves as entry point for JAX-RS
-}
